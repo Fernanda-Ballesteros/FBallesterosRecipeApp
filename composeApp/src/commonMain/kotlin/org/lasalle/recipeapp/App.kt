204c96809b4da80c22a69ebc44ca45985f7ba3ea
Fernanda-Ballesteros <fernanda.bajsm@gmail.com>
2025-11-23 15:26:46 -0600

    Subiendo archivos restantes

--- composeApp/src/commonMain/kotlin/org/lasalle/recipeapp/App.kt
+++ composeApp/src/commonMain/kotlin/org/lasalle/recipeapp/App.kt
@@ -2,16 +2,42 @@ package org.lasalle.recipeapp
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.lasalle.recipeapp.data.services.Preferences
+import org.lasalle.recipeapp.ui.HomeScreenRoute
+import org.lasalle.recipeapp.ui.LoginScreenRoute
 import org.lasalle.recipeapp.ui.RecipeTheme
+import org.lasalle.recipeapp.ui.RegisterScreenRoute
 import org.lasalle.recipeapp.ui.screens.auth.LoginScreen
 import org.lasalle.recipeapp.ui.screens.auth.RegisterScreen
 import org.lasalle.recipeapp.ui.screens.home.HomeScreen
+import org.lasalle.recipeapp.ui.viewmodels.AuthViewModel
 
 @Composable
 @Preview
 fun App() {
     RecipeTheme {
-        HomeScreen()
+        val navController = rememberNavController()
+        val isLogged = Preferences.getIsLogged()
+        NavHost(
+            navController = navController,
+            startDestination = if (isLogged) HomeScreenRoute else LoginScreenRoute
+        ) {
+            composable<LoginScreenRoute> {
+                LoginScreen(navController)
+            }
+
+            composable<RegisterScreenRoute> {
+                RegisterScreen(navController)
+            }
+
+            composable<HomeScreenRoute> {
+                HomeScreen(navController)
+            }
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/org/lasalle/recipeapp/data/services/AuthService.kt
+++ composeApp/src/commonMain/kotlin/org/lasalle/recipeapp/data/services/AuthService.kt
@@ -3,7 +3,9 @@ package org.lasalle.recipeapp.data.services
 import org.lasalle.recipeapp.models.AuthResponse
 import org.lasalle.recipeapp.models.RegisterBody
 import de.jensklingenberg.ktorfit.http.Body
+import de.jensklingenberg.ktorfit.http.GET
 import de.jensklingenberg.ktorfit.http.POST
+import de.jensklingenberg.ktorfit.http.Path
 import org.lasalle.recipeapp.models.LoginBody
 
 interface AuthService {
@@ -12,4 +14,5 @@ interface AuthService {
 
     @POST("auth/login")
     suspend fun login(@Body resquest: LoginBody) : AuthResponse
+
 }
